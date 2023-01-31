6d7224f22e04ffd80fdd2851a7ed35f69359e033
DiceglieA <angelodiceglie10@gmail.com>
2023-01-31 21:50:28 +0100

    seed one to many + creazione categories

--- app/Post.php
+++ app/Post.php
@@ -2,9 +2,22 @@
 
 namespace App;
 
+use App\Traits\Slugger;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    use Slugger;
+
+    public function category() {
+        return $this->belongsTo('App\Category');
+    }
+
+
+    // per usare nei link lo slug anzichè l'id
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
 }
